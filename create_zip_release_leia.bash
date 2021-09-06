71f4aa4b9966f13c123f7e4a4066b69030ee2582
F.-Nicola Demers <fndemers@gmail.com>
2021-09-06 11:41:14 -0400

    Correction des scripts de création de release

--- create_zip_release_leia.bash
+++ create_zip_release_leia.bash
@@ -6,7 +6,7 @@ cd plugin.video.horscine
 cp -f addon.xml addon-matrix.xml
 cp -f addon-leia.xml addon.xml
 cd ..
-zip -r plugin.video.horscine_leia.zip plugin.video.horscine/* -x "*tests-venv*" -x "*tests*" -x "*.bash" -x "*__pycache__*" -x "*addon-matrix.xml" -x "*addon-leia.xml"
+zip -r plugin.video.horscine_leia.zip plugin.video.horscine/* -x "*tests-venv*" -x "*tests*" -x "*.bash" -x "*.pyo" -x "*__pycache__*" -x "*addon-matrix.xml" -x "*addon-leia.xml"
 cd plugin.video.horscine
 rm -f addon.xml
 cp -f addon-matrix.xml addon.xml
--- create_zip_release_matrix.bash
+++ create_zip_release_matrix.bash
@@ -2,4 +2,4 @@
 
 cd ..
 rm -f plugin.video.horscine.zip
-zip -r plugin.video.horscine.zip plugin.video.horscine/* -x "*tests-venv*" -x "*tests*" -x "*.bash" -x "*__pycache__*"
+zip -r plugin.video.horscine.zip plugin.video.horscine/* -x "*tests-venv*" -x "*tests*" -x "*.pyo" -x "*.bash" -x "*__pycache__*" -x "*addon-matrix.xml" -x "*addon-leia.xml"
